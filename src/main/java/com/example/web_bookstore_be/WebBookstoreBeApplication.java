26ecd28891accee08d5e18403940d8b0e5dc1dba
20130009-NguyenQuocDai <125960231+20130009-NguyenQuocDai@users.noreply.github.com>
2024-06-24 02:39:14 +0700

    first commit

---
+++ src/main/java/com/example/web_bookstore_be/WebBookstoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_bookstore_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBookstoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBookstoreBeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/web_bookstore_be/config/appconfig.java
@@ -0,0 +1,4 @@
+package com.example.web_bookstore_be.config;
+
+public class appconfig {
+}
